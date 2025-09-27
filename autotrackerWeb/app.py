e7ae354f0caf3fc269be5ebfaef06edd89619f84
baldlow <147939873+baldlow@users.noreply.github.com>
2025-09-27 18:48:03 -0400

    changes
    getting some new changes

--- autotrackerWeb/app.py
+++ autotrackerWeb/app.py
@@ -1,20 +1,11 @@
 from flask import Flask, request
 from flask import Flask, jsonify
-<<<<<<< Updated upstream
 from flask import Flask, render_template
-=======
-from flask import Flask, render_template 
-
->>>>>>> Stashed changes
 
 app = Flask(__name__)
 @app.route("/")
 def home():
-<<<<<<< Updated upstream
     return render_template("index.html")  # serves templates/index.html
-=======
-    return render_template("index.html")
->>>>>>> Stashed changes
 @app.route('/analyze',methods=["POST"])
 def analyze():
     if "video" not in request.files:
